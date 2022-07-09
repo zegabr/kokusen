de900d0c4a2dc2ce2681f58a1af239c4ab01446d
zegabr <jgsp2@cin.ufpe.br>
2022-07-09 17:50:40 -0300

    add last feature, black flashing

--- lua/kokusen/black_flash.lua
+++ lua/kokusen/black_flash.lua
@@ -1,23 +1,23 @@
 local M = {}
 
-Colors = {
-    current_color = "gruvbox", -- TODO: this should be a constant having the user current color scheme. using vim.cmd[[echo g:colors_name ]] does not seems to return a constant, it is called every time I try to access the variable
-    black_flash_color = "darkblue", -- TODO: get all nvim default colorscheme and choose a random one
-}
-
-local function sleep()
-    local str = "."
-    for _ = 1, 111111 do
-        str = str .. "."
-    end
-end
-
--- TODO: this is not changing color, or it is changing only once
 M.flash_colors = function()
-    sleep()
-    vim.cmd("colorscheme " .. Colors.black_flash_color) -- darkblue
-    -- print(vim.inspect(Colors))
-    vim.cmd("colorscheme " .. Colors.current_color) -- gruvbox
+    vim.cmd[[
+        let current = g:colors_name
+        let flash = 'koehler'
+
+        let i = 0
+        while i < 10
+            let i = i+1
+            execute 'colorscheme ' .. flash
+
+            redraw
+            while g:colors_name != current
+                execute 'colorscheme ' .. current
+            endwhile
+            redraw
+        endwhile
+    ]]
 end
 
 return M
+
--- lua/kokusen/init.lua
+++ lua/kokusen/init.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 M.weak_hit = function ()
+    require("kokusen.black_flash").flash_colors() -- not working
     require("kokusen.music").get_me_to_the_zone()
     print("Kokusen!!")
 end
 
 M.normal_hit = function ()
-    require("kokusen.black_flash").flash_colors() -- not working
     require("kokusen.collapse").collapse_space() -- too strong (and annoying)
     M.weak_hit()
 end
